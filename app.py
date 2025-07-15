95ba4401c8be486359ae5164a56c0b5f4a44bbed
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-07-15 16:26:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "✅ Hello from Flask on GCP VM via Docker & CircleCI!"
+    return "✅ Hello from Flask on GCP MIG & CircleCI!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
